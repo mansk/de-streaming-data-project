05f861afb10b647b00c2c2628ef6b77ee36875b1
mansk <mansk@users.noreply.github.com>
2024-12-21 18:12:33 +0000

    fetch.py: Handle failed api requests

--- src/fetch.py
+++ src/fetch.py
@@ -10,7 +10,7 @@ ENDPOINT = "https://content.guardianapis.com/search"
 API_KEY = os.getenv("GUARDIAN_API_KEY", default="test")
 
 
-def fetch(search_term=None, date_from=None):
+def fetch(search_term: None | str = None, date_from: None | str = None):
     querystring = f"api-key={API_KEY}"
     logstring = f"Fetching results with no search term"
     if search_term:
@@ -25,3 +25,11 @@ def fetch(search_term=None, date_from=None):
 
     if response.status_code == 200:
         return response.json()["response"]["results"]
+    elif response.status_code == 401:
+        logging.error(
+            f"Failed to fetch results: status code 401. API key {API_KEY} may be invalid"
+        )
+    else:
+        logging.error(f"Failed to fetch results: status code {response.status_code}")
+        if error_message:=response.json()["response"]["message"]:
+            logging.error(f"Server returned error message: {error_message}")
